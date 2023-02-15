fcc83c6b3e4ad0a9e4f0a08a279c94d13243287a
RicardoHungJS <ricardohung92@gmail.com>
2023-02-14 20:54:05 -0500

    primer comentario

---
+++ src/components/landing/landing.tsx
@@ -0,0 +1,15 @@
+import { FC, useEffect, useState } from "react";
+import './landing.scss';
+
+interface IProps {
+}
+
+const LandingComponent: FC<IProps> = () => {
+    return (
+        <div className="landing-component-container">
+
+        </div>
+    );
+};
+
+export default LandingComponent;
